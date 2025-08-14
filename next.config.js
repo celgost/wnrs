f8b00f8e95fcc0f9b0e041b0d87be4035f5a7f4c
celgost <brice.basty1@gmail.com>
2025-08-14 13:43:56 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withPWA = require('next-pwa')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = withPWA({
   reactStrictMode: true,
-  // pwa: { disable: true }
+  // Configure for GitHub Pages subdirectory
+  basePath: isProd ? '/wnrs' : '',
+  assetPrefix: isProd ? '/wnrs/' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  pwa: { disable: true }  // Disable PWA for now
 })
